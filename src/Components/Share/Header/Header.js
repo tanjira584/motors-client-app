5f68f5bd434fedd7f3a8c90ed44d0f98e6146640
Tanjir Ahmed <tanjira584@gmail.com>
2022-06-02 05:42:30 +0600

    Header Top Added

--- src/Components/Share/Header/Header.js
+++ src/Components/Share/Header/Header.js
@@ -6,95 +6,104 @@ import "./Header.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
 import { signOut } from "firebase/auth";
+import HeaderTop from "./HeaderTop";
 
 const Header = () => {
     const [auser] = useAuthState(auth);
     const navigate = useNavigate();
     return (
-        <div
-            className="header-main"
-            style={{ backgroundImage: `url(${headerbg})` }}
-        >
-            <nav className="navbar navbar-expand-lg navbar-dark">
-                <div className="container-md">
-                    <Link to="/" className="navbar-brand">
-                        <img src={logo} alt="" />
-                    </Link>
-                    <button
-                        className="navbar-toggler text-light"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon text-light"></span>
-                    </button>
-                    <div
-                        className="collapse navbar-collapse"
-                        id="navbarSupportedContent"
-                    >
-                        <ul className="navbar-nav main-menu ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link to="/" className="nav-link active">
-                                    Home
-                                </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/inventory" className="nav-link">
-                                    Inventory
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/dealers" className="nav-link">
-                                    dealers
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/blogs" className="nav-link">
-                                    Blogs
-                                </Link>
-                            </li>
-
-                            {auser && (
+        <header>
+            <HeaderTop></HeaderTop>
+            <div
+                className="header-main"
+                style={{ backgroundImage: `url(${headerbg})` }}
+            >
+                <nav className="navbar navbar-expand-lg navbar-dark">
+                    <div className="container-md">
+                        <Link to="/" className="navbar-brand">
+                            <img src={logo} alt="" />
+                        </Link>
+                        <button
+                            className="navbar-toggler text-light"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon text-light"></span>
+                        </button>
+                        <div
+                            className="collapse navbar-collapse"
+                            id="navbarSupportedContent"
+                        >
+                            <ul className="navbar-nav main-menu ms-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link to="/my-items" className="nav-link">
-                                        My Items
+                                    <Link to="/" className="nav-link active">
+                                        Home
                                     </Link>
                                 </li>
-                            )}
-                            {auser && (
+
                                 <li className="nav-item">
-                                    <button
-                                        onClick={() => signOut(auth)}
-                                        className="nav-link bg-transparent border-0"
-                                    >
-                                        Signout
-                                    </button>
+                                    <Link to="/inventory" className="nav-link">
+                                        Inventory
+                                    </Link>
                                 </li>
-                            )}
-                            <li className="nav-item">
-                                <Link to="/login" className="nav-link">
-                                    Login/Register
-                                </Link>
-                            </li>
-                            {auser && (
                                 <li className="nav-item">
-                                    <button
-                                        onClick={() => navigate("/add-item")}
-                                        className="add-item-btn"
-                                    >
-                                        Add Item
-                                    </button>
+                                    <Link to="/dealers" className="nav-link">
+                                        dealers
+                                    </Link>
                                 </li>
-                            )}
-                        </ul>
+                                <li className="nav-item">
+                                    <Link to="/blogs" className="nav-link">
+                                        Blogs
+                                    </Link>
+                                </li>
+
+                                {auser && (
+                                    <li className="nav-item">
+                                        <Link
+                                            to="/my-items"
+                                            className="nav-link"
+                                        >
+                                            My Items
+                                        </Link>
+                                    </li>
+                                )}
+                                {auser && (
+                                    <li className="nav-item">
+                                        <button
+                                            onClick={() => signOut(auth)}
+                                            className="nav-link bg-transparent border-0"
+                                        >
+                                            Signout
+                                        </button>
+                                    </li>
+                                )}
+                                <li className="nav-item">
+                                    <Link to="/login" className="nav-link">
+                                        Login/Register
+                                    </Link>
+                                </li>
+                                {auser && (
+                                    <li className="nav-item">
+                                        <button
+                                            onClick={() =>
+                                                navigate("/add-item")
+                                            }
+                                            className="add-item-btn"
+                                        >
+                                            Add Item
+                                        </button>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
+                </nav>
+            </div>
+        </header>
     );
 };
 
---
+++ src/Components/Share/Header/HeaderTop.js
@@ -0,0 +1,82 @@
+import {
+    faFacebook,
+    faInstagram,
+    faLinkedinIn,
+    faPinterestP,
+    faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./Header.css";
+
+const HeaderTop = () => {
+    return (
+        <div className="header-top">
+            <div className="container-md">
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="text-light">
+                        <select
+                            className="text-secondary bg-transparent border-0"
+                            name=""
+                            id=""
+                        >
+                            <option value="">English</option>
+                            <option value="">France</option>
+                            <option value="">Bangla</option>
+                        </select>
+                    </div>
+                    <div className="d-flex align-items-center">
+                        <div className="contact text-secondary">
+                            <p className="m-0">
+                                <FontAwesomeIcon
+                                    icon={faPhone}
+                                ></FontAwesomeIcon>
+                                <span className="d-inline-block ms-2">
+                                    +88001613071257
+                                </span>
+                            </p>
+                        </div>
+                        <div className="address ms-5 text-secondary">
+                            <p className="m-0">Brahmanbaria, Kasba , Deli</p>
+                        </div>
+                        <div className="opening-time ms-5  text-secondary">
+                            <p className="m-0"> MON - SAT 8.00 - 18.00</p>
+                        </div>
+                        <div className="header-social ms-5">
+                            <ul className="m-0 p-0 d-flex">
+                                <li>
+                                    <FontAwesomeIcon
+                                        icon={faFacebook}
+                                    ></FontAwesomeIcon>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon
+                                        icon={faTwitter}
+                                    ></FontAwesomeIcon>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon
+                                        icon={faInstagram}
+                                    ></FontAwesomeIcon>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon
+                                        icon={faLinkedinIn}
+                                    ></FontAwesomeIcon>
+                                </li>
+                                <li>
+                                    <FontAwesomeIcon
+                                        icon={faPinterestP}
+                                    ></FontAwesomeIcon>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderTop;
